7d9b50c7c15d2238dcab8c0f76142fe4c5c87d5a
Amol Deshmukh <amol.deshmukh@idmission.com>
2023-03-11 16:39:49 +0530

    Updated Documentation for SDK v9.4.8.2.1

---
+++ push-all.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#Push All PodSpec file to CocoapodsTrunk
+pod trunk push FingerPrintCaptureLite.podspec
+pod trunk push IDCaptureLite.podspec
+pod trunk push IDentityLiteSDK2.0.podspec
+pod trunk push SelfieCaptureLite.podspec
+pod trunk push SignatureCaptureLite.podspec
